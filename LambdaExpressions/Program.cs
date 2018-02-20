2fbc04b578391fd6f0f3f8a0d1afe6743ecbbb88
Rachel Stanhope <restanhope@outlook.com>
2018-02-20 15:16:23 -0600

    Add project files.

---
+++ LambdaExpressions/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        #region StaticFields
+        private static List<int> _integers = new List<int> { 3, 1, 4, 1, 5, 9, 2, 6, 5, 9 };
+
+        private static List<string> _strings = new List<string> {
+            "Hello",
+            "World!",
+            "I",
+            "am",
+            "a",
+            "list",
+            "of",
+            "strings",
+        };
+
+        #endregion
+
+        #region StaticMethods
+        static int Aggregate(Func<int, int, int> f, List<int> list)
+        {
+            int result = list[0];
+            for (int i = 1; i < list.Count; ++i)
+            {
+                result = f(result, list[i]);
+            }
+
+            return result;
+        }
+
+        static IEnumerable<int> Select(Func<int, int> f, IEnumerable<int> list)
+        {
+            List<int> result = new List<int>();
+            foreach (int val in list)
+            {
+                result.Add(f(val));
+            }
+            return result;
+        }
+
+        static void ForEach<T>(Action<T> f, IEnumerable<T> list)
+        {
+            foreach (T val in list)
+            {
+                f(val);
+            }
+        }
+
+        #endregion
+
+        static int Sum(int x, int y)
+        {
+            int z = x + y;
+            return z;
+        }
+
+        static int Multiply(int a, int b)
+        {
+            int c = a * b;
+            return c;
+        }
+
+        static int MakeDouble(int d)
+        {
+            int e = d * 2;
+            return e;
+        }
+
+        static int Square(int g)
+        {
+            int h = g * g;
+            return h;
+        }
+
+        static void Main(string[] args)
+        {
+            int a = Sum(5, 7);
+            Console.WriteLine("Result of Aggregate = {0}", a);
+            Console.WriteLine(a);
+            Console.ReadLine();
+
+            int Aggregate = Multiply(5, 7);
+            Console.WriteLine("Result of Aggregate = {0}", Aggregate);
+            Console.ReadLine();
+
+            //Console.WriteLine(Aggregate((p, q) => p + q, new List<int>()));
+
+            // Console.WriteLine(Aggregate((p, q) => p * q, new List<int>()));
+
+            int Select =  MakeDouble(5);
+            Console.WriteLine("Result of MakeDouble is {0}", Select);
+
+            //int Select = Square(5);
+            //Console.WriteLine("Result of Square is {0}", Select);
+
+            //Console.WriteLine(Select((k) => k * 2, new List<int>()));
+
+            //Console.WriteLine(Select((k) => k * k, new List<int>()));
+
+            ForEach<int>(x => Console.Write(x), _integers);
+
+            ForEach<string>(x => Console.Write(x), _strings);
+        }
+    }
+}
